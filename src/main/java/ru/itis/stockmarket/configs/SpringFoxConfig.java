f5009b945e22d58469362ba275796956ce512cb2
lordvidex <lordvidex.prime@gmail.com>
2022-05-01 08:19:36 +0300

    added swagger docs that works

---
+++ src/main/java/ru/itis/stockmarket/configs/SpringFoxConfig.java
@@ -0,0 +1,42 @@
+package ru.itis.stockmarket.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by IntelliJ IDEA
+ * Date: 30.04.2022
+ * Time: 11:56 PM
+ *
+ * @author lordvidex
+ * Name: Овамойо Олувадамилола Эванс
+ * <p>
+ * Desc:
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Stock API")
+                .build();
+    }
+}
