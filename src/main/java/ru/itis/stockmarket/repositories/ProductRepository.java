74282117678d26ade891ebe7ec181c46e6a2afbc
lordvidex <lordvidex.prime@gmail.com>
2022-05-04 11:58:22 +0300

    updated ProductRepository

--- src/main/java/ru/itis/stockmarket/repositories/ProductRepository.java
+++ src/main/java/ru/itis/stockmarket/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package ru.itis.stockmarket.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itis.stockmarket.models.ProductTag;
+import ru.itis.stockmarket.models.Product;
 
 
-public interface ProductRepository extends JpaRepository<ProductTag, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
