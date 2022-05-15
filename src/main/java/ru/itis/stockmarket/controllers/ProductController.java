ca7f600c337b7e8eab01b5271de5d737af2bed3f
ali <aliaqel2021@gmail.com>
2022-05-15 19:20:50 +0300

    added createProduct to ProductController

--- src/main/java/ru/itis/stockmarket/controllers/ProductController.java
+++ src/main/java/ru/itis/stockmarket/controllers/ProductController.java
@@ -1,23 +1,42 @@
 package ru.itis.stockmarket.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.itis.stockmarket.dtos.ProductRequestDto;
+import ru.itis.stockmarket.dtos.*;
 import ru.itis.stockmarket.models.Product;
 import ru.itis.stockmarket.services.ProductService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
 
-    private final ProductService productService;
+    private final ProductService<ProductRequestDto,ProductResponseDto> productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService<ProductRequestDto, ProductResponseDto> productService) {
         this.productService = productService;
+    }
+
+
+    @PostMapping
+    ResponseEntity<InnerIdResponseDto> createProduct(@Valid @RequestBody ProductRequestDto product) {
+        ProductResponseDto serviceProduct = productService.createProduct(product);
+        InnerIdResponseDto controllerResponse;
+
+        controllerResponse = InnerIdResponseDto
+                .builder()
+                .innerid(serviceProduct.getInnerId())
+                .status(Status.success)
+                .description("Product created successfully")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(controllerResponse);
     }
 
     @PostMapping(path = "/list")
