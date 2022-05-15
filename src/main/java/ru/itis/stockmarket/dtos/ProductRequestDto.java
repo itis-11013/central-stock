0a257e0990e02a5473f316d9fada82fc2e7bdb98
ali <aliaqel2021@gmail.com>
2022-05-15 19:21:14 +0300

    added productDto

--- src/main/java/ru/itis/stockmarket/dtos/ProductRequestDto.java
+++ src/main/java/ru/itis/stockmarket/dtos/ProductRequestDto.java
@@ -1,12 +1,28 @@
 package ru.itis.stockmarket.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.groups.Default;
+import java.util.UUID;
 
 
 @Data
 @NoArgsConstructor
 public class ProductRequestDto {
-   private String code;
-   private String count;
+    @NotEmpty(groups = {Default.class, OnCreate.class})
+    private String code;
+    @NotEmpty(groups = {Default.class, OnCreate.class})
+    private String name;
+    @JsonProperty(value = "sellerid")
+    private UUID sellerId;
+    @DecimalMin("1.0")
+    private double count;
+    @NotEmpty(groups = {Default.class, OnCreate.class})
+    private String unit;
+    @DecimalMin("1.0")
+    private double price;
 }
--- src/main/java/ru/itis/stockmarket/repositories/ProductRepository.java
+++ src/main/java/ru/itis/stockmarket/repositories/ProductRepository.java
@@ -1,13 +1,12 @@
 package ru.itis.stockmarket.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.itis.stockmarket.models.Product;
-
-import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCatalog_Code(String code);
+    Optional<Product> existsProductBySellerIdAndName(UUID id, String name);
+
 }
