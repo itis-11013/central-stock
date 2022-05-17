292234669b7e9d22ffd8b2906893c8ce2a339345
Owamoyo Evans <miles9morales@gmail.com>
2022-05-18 00:01:18 +0300

    replaced Springfox with newer Springdoc

--- src/main/java/ru/itis/stockmarket/StockMarketApplication.java
+++ src/main/java/ru/itis/stockmarket/StockMarketApplication.java
@@ -6,11 +6,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableWebMvc
+//@EnableWebMvc
 public class StockMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockMarketApplication.class, args);
