fbe758abe7efbf465a7fb13f4c3f645d0d34eea1
Owamoyo Evans <miles9morales@gmail.com>
2022-06-06 01:59:47 +0300

    added spring boot tests

--- src/main/java/ru/itis/stockmarket/configs/SecurityConfig.java
+++ src/main/java/ru/itis/stockmarket/configs/SecurityConfig.java
@@ -1,8 +1,10 @@
 package ru.itis.stockmarket.configs;
 
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
